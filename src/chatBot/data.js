394a20b9c95208aff73b2b640b65b3fafb989b68
Rahul Gupta <rahulgupta6542@gmail.com>
2021-09-30 15:30:40 +0530

    commit

---
+++ src/chatBot/data.js
@@ -0,0 +1,101 @@
+export const data = [
+  {
+    question: 'facilities available',
+    answer: `
+    Please Select any option :
+      f1. Credit
+      f2. Over Draft
+      f3. NEFT
+      f4. Bank Draft
+      f5. RTGS
+    `,
+  },
+  {
+    question: 'f1',
+    answer: 'Types of credit facilities include revolving loan facilities, retail credit facilities (like credit cards), committed facilities, letters of credit, and most retail credit accounts.',
+  },
+  {
+    question: 'f2',
+    answer: 'An overdraft account is operated in the same way as a current account. In overdraft the interest is charged on the credit actually utilised, i.e. to the extent amount is overdrawn.',
+  },
+  {
+    question: 'f3',
+    answer: 'NEFT facility is open even to those, who do not have bank account. The persons without bank accounts can deposit cash at the NEFT-enabled branch with instructions to transfer funds using NEFT. A separate Transaction Code (No. 50) has been allotted in the NEFT system to facilitate walk-in-customers to deposit cash and transfer funds to the beneficiary.',
+  },
+  {
+    question: 'f4',
+    answer: 'When a customer wants to send money to other places then he will have to fill a specific proforma for this purpose. The name of the person/party to whom the amount is to be sent, the amount for which the draft is required, the place for which the draft is required, bank charges are mentioned in the proforma.',
+  },
+  {
+    question: 'f5',
+    answer: 'Customer can access RTGS facility during bank timings. ',
+  },
+  {
+    question: 'neft or rtgs',
+    answer: 'RTGS is a high value transfer system, handling funds worth Rs, 1, 00,000 and above, while NEFT transfers smaller amounts below Rs. 10,000.',
+  },
+  {
+    question: 'limit for neft',
+    answer: 'There is no minimum or maximum limit for fund transfer in NEFT system',
+  },
+  {
+    question: 'loan',
+    answer: 'We have different types of loan, Sir please visit to nearest branch to get the detailed information',
+  },
+  {
+    question: 'new cheque book request',
+    answer: 'Please login to our banking app and sent us a request for new cheque book under requests section',
+  },
+  {
+    question: 'account balance',
+    answer: 'Please Download the passbook app of our bank to get the detailed transactioon history',
+  },
+  {
+    question: 'card lost',
+    answer: 'Please visit to our bank website and report in the security section, so they can get your card blocked',
+  },
+  {
+    question: 'hi',
+    answer: 'Hello, Warm Wishes. How may i help you',
+  },
+  {
+    question: 'hii',
+    answer: 'Hello, Warm Wishes. How may i help you',
+  },
+  {
+    question: 'heyy',
+    answer: 'Hello, Warm Wishes. How may i help you',
+  },
+  {
+    question: 'heyyy',
+    answer: 'Hello, Warm Wishes. How may i help you',
+  },
+  {
+    question: 'Thank You',
+    answer: 'You are most welcome!',
+  },
+  {
+    question: 'good morning',
+    answer: 'Good Morning!',
+  },
+  {
+    question: 'good afternoon',
+    answer: 'Good Afternoon!',
+  },
+  {
+    question: 'good evening',
+    answer: 'Good Evening!',
+  },
+  {
+    question: 'good night',
+    answer: 'Good Night!',
+  },
+  {
+    question: 'thank you',
+    answer: 'You are most welcome!',
+  },
+  {
+    question: 'ty',
+    answer: 'You are most welcome!',
+  },
+];
